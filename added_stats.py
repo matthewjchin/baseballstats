43c757e4fff434bc9076b41eee69702a71df1521
Matthew Chin <mattchin813@gmail.com>
2021-12-31 22:27:12 -0800

    added BP28 datavis & code

--- added_stats.py
+++ added_stats.py
@@ -45,20 +45,34 @@ sluggers = slugpct.to_numpy()
 
 posey_stats = [None] * 11
 counter = 0
+posey_hr = [None] * 11
+posey_yr = [None] * 11
+posey_ba = [None] * 11
 
 for a in sluggers:
     if a[0] == 'poseybu01':
         posey_stats[counter] = a
-        counter += 1
+        posey_hr[counter] = a[12]
+        posey_yr[counter] = a[1]
+        posey_ba[counter] = a[8] / a[6]
         # print(a)
+        counter += 1
+        
 
-print(posey_stats)
+# posey_yr = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
-# print(slugpct.yearID == 2012)
 
-# Print Brandon Crawford's stats, 2011-2020 seasons
-# for a in sluggers:
-#     if a[0] == 'crawfbr01':
-#         print(a)
+plt.bar(posey_yr, posey_hr)
+plt.title("Buster Posey HR Count, 2009-2019")
+plt.xlabel("Season of Play")
+plt.ylabel("Home Runs Hit")
+plt.show()
 
-# import csv
+
+plt.plot(posey_yr, posey_ba)
+plt.title("Buster Posey Batting Average, 2009-2019")
+plt.xlabel("Season of Play")
+plt.ylabel("Batting Average")
+plt.show()
+
+
